1e01181447ae1413e251df480482ad992bdaccfd
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-11-23 15:56:29 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,15 +7,26 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+#Selects * From Projects.title and Pledge.amount sorted by name
+"SELECT title, SUM(pledges.amount) FROM projects
+INNER JOIN pledges ON pledges.project_id = projects.id
+GROUP BY projects.title
+ORDER BY projects.title; "
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+#SELECT users.name, users.age & pledges.amount for all pledges by name
+"SELECT name, age, SUM(pledges.amount) FROM users
+JOIN pledges ON pledges.user_id = users.id
+GROUP BY users.name
+ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+#SELECT projects.title and amount over goal of projects that met goal
+"SELECT title, SUM(pledges.amount - projects.funding_goal) FROM projects
+JOIN pledges ON pledges.project_id = projects.id
+GROUP BY projects.title HAVING SUM(pledges.amount - projects.funding_goal) > 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
